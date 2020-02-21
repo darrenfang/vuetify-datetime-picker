a39233bffeb998f84d95b0d389afb5a7bacdfe84
Kenny Mochizuki <kenny.mochizuki@gmail.com>
2020-02-20 21:23:53 -0600

    Fixing import error on index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@
  *
  */
 
-import DatetimePicker from './components/DatetimePicker'
+import DatetimePicker from './components/DatetimePicker.vue'
 
 const install = Vue => {
   Vue.component('v-datetime-picker', DatetimePicker)
